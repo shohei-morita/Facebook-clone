02d756d056790f0bc354281bccf04748fb0647d3
shohei morita <shomorita1985@gmail.com>
2020-04-26 13:06:26 +0900

    picturesコントローラー作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'pictures/index'
+  get 'pictures/new'
   get 'users/new'
   get 'users/show'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
